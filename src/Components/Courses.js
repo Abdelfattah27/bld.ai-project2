a675a2cc7b08ef9cc3b772829c75a0fdcf177d4d
abdelfattah27 <Abdelfattah.hamdy234@gmail.com>
2022-09-02 14:01:10 +0200

    few changes to old phase two

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -3,37 +3,82 @@ import Card from "./Card";
 import Styles from "./Styles.module.css";
 
 function Courses() {
+  const [loading, setLoading] = useState(true);
   const [courses, setCourses] = useState([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8000/courses")
       .then((response) => response.json())
-      .then((data) => {
-        setCourses(data);
+      .then((response) => {
+        setLoading(false);
+        setCourses(response);
         setError("");
       })
       .catch((err) => {
-        setError(err);
+        setLoading(false);
+        setError("Failed to load Courses");
         setCourses([]);
       });
-  }, [error]);
-  return error ? (
-    <div className={Styles.error}>Failed to load Courses 😟😟</div>
-  ) : courses.length > 0 ? (
-    <div className={Styles.courses}>
-      {courses.map((course) => (
-        <Card key={course.id}>{course}</Card>
-      ))}
-    </div>
-  ) : (
-    <div className={Styles.loading}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+  }, []);
+
+  let renderState = [];
+  if (error) {
+    renderState = (
+      <div className={Styles.error}>Failed to load Courses 😟😟</div>
+    );
+  } else if (loading || courses.length === 0) {
+    renderState = (
+      <div className={Styles.loading}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  } else {
+    renderState = (
+      <div className={Styles.courses}>
+        {courses.map((course) => (
+          <Card key={course.id}>{course}</Card>
+        ))}
+      </div>
+    );
+  }
+  return renderState;
+  // return (
+  //   <>
+  //     {loading ? (
+  //       <div className={Styles.loading}>
+  //         <div></div>
+  //         <div></div>
+  //         <div></div>
+  //         <div></div>
+  //       </div>
+  //     ) : error ? (
+  //       <div className={Styles.error}>Failed to load Courses 😟😟</div>
+  //     ) : (
+  //       <div className={Styles.courses}>
+  //         {courses.map((course) => (
+  //           <Card key={course.id}>{course}</Card>
+  //         ))}
+  //       </div>
+  //     )}
+  //   </>
+  // );
+
+  // return error ? (
+  //   <div className={Styles.error}>Failed to load Courses 😟😟</div>
+  // ) : courses.length > 0 ? (
+
+  // ) : (
+  //   <div className={Styles.loading}>
+  //     <div></div>
+  //     <div></div>
+  //     <div></div>
+  //     <div></div>
+  //   </div>
+  // );
 }
 
 export default Courses;
